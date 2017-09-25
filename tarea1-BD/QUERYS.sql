78fa9253613b6da56685271bf6123e209a3beaa0
Andres Tapia <andres.tapia@sansano.usm.cl>
2017-09-24 23:33:48 -0300

    Se agregaron todas las querys

--- tarea1-BD/QUERYS.sql
+++ tarea1-BD/QUERYS.sql
@@ -3,16 +3,23 @@ select titulo from Curso where correoProfesor='cecilia.reyes@usm.cl' ;
 select titulo from Curso where (FechaInicioCurso>='2017')&(FechaFinCurso<'2018') ;
 select Estudiante.nombre from Estudiante INNER JOIN Curso_Estudiante ON (Curso_Estudiante.idCurso=2)&(Curso_Estudiante.correoEstudiante=Estudiante.correoEstudiante);
 
-
-select distinct Estudiante.nombre, Estudiante.apellido, Curso.idCurso
-from Estudiante
-INNER JOIN Curso_Estudiante on (Estudiante.correoEstudiante = Curso_Estudiante.correoEstudiante)
-INNER JOIN Curso on (Curso_Estudiante.idCurso = Curso.idCurso)
-where  Curso.correoProfesor NOT REGEXP '.*@usm.cl';
+select Profesor.nombre, Profesor.apellido, Curso.correoProfesor, COUNT(Curso.idCurso)
+from Curso 
+INNER JOIN Profesor on
+(Profesor.correoProfesor=Curso.correoProfesor)
+GROUP BY Curso.correoProfesor
+HAVING count(*) > 1
 
 select DISTINCT Curso.titulo, Profesor.nombre, Profesor.apellido ,COUNT(Video.idCurso)
 from Video
 INNER JOIN Curso on Curso.idCurso = Video.idCurso
 INNER JOIN Profesor on Curso.correoProfesor = Profesor.correoProfesor
 GROUP BY Video.idCurso;
+
+select DISTINCT Estudiante.nombre, Estudiante.apellido, Curso.idCurso
+from Estudiante
+INNER JOIN Curso_Estudiante on (Estudiante.correoEstudiante = Curso_Estudiante.correoEstudiante)
+INNER JOIN Curso on (Curso_Estudiante.idCurso = Curso.idCurso)
+where  Curso.correoProfesor NOT REGEXP '.*@usm.cl';
+
 
