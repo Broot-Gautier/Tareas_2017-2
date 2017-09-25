29a5c45b706691e0c7db570ac0277967ef423879
Andres Tapia <andres.tapia@sansano.usm.cl>
2017-09-24 23:10:39 -0300

    se agrega 7tima query

--- tarea1-BD/QUERYS.sql
+++ tarea1-BD/QUERYS.sql
@@ -2,3 +2,10 @@ select nombre from Profesor;
 select titulo from Curso where correoProfesor='cecilia.reyes@usm.cl' ;
 select titulo from Curso where (FechaInicioCurso>='2017')&(FechaFinCurso<'2018') ;
 select Estudiante.nombre from Estudiante INNER JOIN Curso_Estudiante ON (Curso_Estudiante.idCurso=2)&(Curso_Estudiante.correoEstudiante=Estudiante.correoEstudiante);
+
+
+select distinct Estudiante.nombre, Estudiante.apellido, Curso.idCurso
+from Estudiante
+INNER JOIN Curso_Estudiante on (Estudiante.correoEstudiante = Curso_Estudiante.correoEstudiante)
+INNER JOIN Curso on (Curso_Estudiante.idCurso = Curso.idCurso)
+where  Curso.correoProfesor NOT REGEXP '.*@usm.cl';
